dc2bd4ce0fb0602e0b677f550c956a521d1fecb6
Abishek-55 <avisgek.pau@gmail.com>
2026-03-10 13:41:09 +0545

    Finish project

---
+++ scrape.py
@@ -0,0 +1,41 @@
+import json
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+
+def scrape_books(url):
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return
+
+    # set encoding explicitly to handle special characters correctly
+
+    response.encoding= response.apparent_encoding
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all("article", class_="product_pod")
+    all_books=[]
+    for book in books:
+        title = book.h3.a["title"]
+        print(title)
+        price_text= book.find('p', class_='price_color').text
+        currency=price_text[0]
+        price= float(price_text[1:])
+        all_books.append(
+            {
+                'title':title,
+                'price': price,
+                'currency': currency
+            }
+        )
+    return all_books
+all_books= scrape_books(url)
+
+with open("books.json", 'w', encoding="utf-8") as f:
+    json.dump(all_books, f, indent=4, ensure_ascii=False)
+
+# with open('books.csv', 'w', encoding='utf-8') as f:
+#     writer= csv.DictWriter(f, fieldnames=['title', 'price','currency'])
